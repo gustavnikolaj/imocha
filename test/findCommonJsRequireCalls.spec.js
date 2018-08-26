065ad47317222bbcce18bfc6fa4c64bcf9bf9d51
Gustav Nikolaj Olsen <gno@one.com>
2018-08-26 19:23:06 +0200

    Replace esprima with @babel/parser and @babel/traverse

--- test/findCommonJsRequireCalls.spec.js
+++ test/findCommonJsRequireCalls.spec.js
@@ -36,7 +36,7 @@ describe("findCommonJsRequireCalls", () => {
       () => findCommonJsRequireCalls(source),
       "to throw",
       "Can only parse require calls with literal values.\n\n    " +
-        "require('./some' + '-file')"
+        "\n      require('./some' + '-file');\n    "
     );
   });
 
@@ -49,7 +49,7 @@ describe("findCommonJsRequireCalls", () => {
       () => findCommonJsRequireCalls(source),
       "to throw",
       "Cannot parse require calls with more than one argument.\n\n    " +
-        "require('./some', '-file')"
+        "\n      require('./some', '-file');\n    "
     );
   });
 });
