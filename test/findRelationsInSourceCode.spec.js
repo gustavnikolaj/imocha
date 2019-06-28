616ea5d3a33b168d07465bf30152b872fbff063b
Gustav Nikolaj Olsen <gno@one.com>
2019-06-28 12:18:54 +0200

    Remove check against malformed require
    We are now only looking for require calls that match our regexp and thus
    won't find these instances.

--- test/findRelationsInSourceCode.spec.js
+++ test/findRelationsInSourceCode.spec.js
@@ -27,32 +27,6 @@ describe("findRelationsInSourceCode", () => {
     );
   });
 
-  it("should reject non literal arguments to require", () => {
-    const source = `
-      require('./some' + '-file');
-    `;
-
-    expect(
-      () => findRelationsInSourceCode(source),
-      "to throw",
-      "Can only parse require calls with literal values.\n\n    " +
-        "\n      require('./some' + '-file');\n    "
-    );
-  });
-
-  it("should reject multiple arguments to require", () => {
-    const source = `
-      require('./some', '-file');
-    `;
-
-    expect(
-      () => findRelationsInSourceCode(source),
-      "to throw",
-      "Cannot parse require calls with more than one argument.\n\n    " +
-        "\n      require('./some', '-file');\n    "
-    );
-  });
-
   it("should return a list of imported files", () => {
     expect(
       findRelationsInSourceCode(`
