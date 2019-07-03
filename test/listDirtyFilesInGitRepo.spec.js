4a20447c94bfd46f4a780851bcc63a6c156bec96
Gustav Nikolaj Olsen <gno@one.com>
2019-07-03 10:56:56 +0200

    Move runShellCommand into listDirtyFilesInGitRepo

--- test/listDirtyFilesInGitRepo.spec.js
+++ test/listDirtyFilesInGitRepo.spec.js
@@ -1,5 +1,7 @@
 const expect = require("unexpected");
 const listDirtyFilesInGitRepo = require("../lib/listDirtyFilesInGitRepo");
+const path = require("path");
+const { runShellCommand } = listDirtyFilesInGitRepo;
 
 describe("listDirtyFilesInGitRepo", () => {
   let originalRunShellCommand = listDirtyFilesInGitRepo.runShellCommand;
@@ -36,5 +38,53 @@ describe("listDirtyFilesInGitRepo", () => {
       "to be fulfilled with",
       ["/fakeroot/package.json", "/fakeroot/yarn.lock"]
     );
+  });
+  describe("runShellCommand", () => {
+    it("should be a function", () => {
+      expect(runShellCommand, "to be a function");
+    });
+
+    it("should run a simple echo command", () => {
+      return expect(() => runShellCommand("echo foo"), "to be fulfilled with", {
+        error: null,
+        stderr: "",
+        stdout: "foo\n"
+      });
+    });
+
+    describe("options.cwd", () => {
+      it("should set cwd to __dirname", () => {
+        return expect(
+          () => runShellCommand("pwd", { cwd: __dirname }),
+          "to be fulfilled with",
+          {
+            error: null,
+            stderr: "",
+            stdout: `${__dirname}\n`
+          }
+        );
+      });
+
+      it("should set cwd to the parent of __dirname", () => {
+        const parentDir = path.resolve(__dirname, "..");
+        return expect(
+          () => runShellCommand("pwd", { cwd: parentDir }),
+          "to be fulfilled with",
+          {
+            error: null,
+            stderr: "",
+            stdout: `${parentDir}\n`
+          }
+        );
+      });
+
+      it("should default to process.cwd()", () => {
+        return expect(() => runShellCommand("pwd"), "to be fulfilled with", {
+          error: null,
+          stderr: "",
+          stdout: `${process.cwd()}\n`
+        });
+      });
+    });
   });
 });
