9b441fafb95aa827ba82b801f2571bd70beb1582
Gustav Nikolaj Olsen <gno@one.com>
2019-07-03 17:02:39 +0200

    Remove async await from SourceGraph

--- test/SourceGraph.spec.js
+++ test/SourceGraph.spec.js
@@ -12,69 +12,101 @@ describe("SourceGraph", () => {
   });
 
   describe("fixtures", () => {
-    it("should load the simple fixture", async () => {
-      const fixturePath = resolveFixture("simple");
-      const sourceGraph = new SourceGraph(fixturePath);
+    describe("simple fixture", () => {
+      it("should load all three files", () => {
+        const fixturePath = resolveFixture("simple");
+        const sourceGraph = new SourceGraph(fixturePath);
 
-      // pass the list of test files mocha would run to populate
-      await sourceGraph.populate([
-        path.resolve(fixturePath, "test/bar.spec.js")
-      ]);
-
-      await expect(sourceGraph, "to satisfy", {
-        files: [
-          { path: path.resolve(fixturePath, "test/bar.spec.js") },
-          { path: path.resolve(fixturePath, "bar.js") },
-          { path: path.resolve(fixturePath, "foo.js") }
-        ]
+        return expect(
+          () =>
+            sourceGraph.populate([
+              path.resolve(fixturePath, "test/bar.spec.js")
+            ]),
+          "to be fulfilled"
+        ).then(() => {
+          expect(sourceGraph, "to satisfy", {
+            files: [
+              { path: path.resolve(fixturePath, "test/bar.spec.js") },
+              { path: path.resolve(fixturePath, "bar.js") },
+              { path: path.resolve(fixturePath, "foo.js") }
+            ]
+          });
+        });
       });
 
-      await expect(
-        sourceGraph.query({
-          type: "relations",
-          to: path.resolve(fixturePath, "foo.js")
-        }),
-        "to satisfy",
-        [
-          {
-            from: path.resolve(fixturePath, "bar.js"),
-            to: path.resolve(fixturePath, "foo.js")
-          }
-        ]
-      );
+      it("should find code depending on foo.js", () => {
+        const fixturePath = resolveFixture("simple");
+        const sourceGraph = new SourceGraph(fixturePath);
 
-      await expect(
-        sourceGraph.query({
-          type: "relations",
-          to: path.resolve(fixturePath, "bar.js")
-        }),
-        "to satisfy",
-        [
-          {
-            from: path.resolve(fixturePath, "test/bar.spec.js"),
-            to: path.resolve(fixturePath, "bar.js")
-          }
-        ]
-      );
+        return expect(
+          () =>
+            sourceGraph.populate([
+              path.resolve(fixturePath, "test/bar.spec.js")
+            ]),
+          "to be fulfilled"
+        ).then(() => {
+          expect(
+            sourceGraph.query({
+              type: "relations",
+              to: path.resolve(fixturePath, "foo.js")
+            }),
+            "to satisfy",
+            [
+              {
+                from: path.resolve(fixturePath, "bar.js"),
+                to: path.resolve(fixturePath, "foo.js")
+              }
+            ]
+          );
+        });
+      });
+      it("should find code depending on bar.js", () => {
+        const fixturePath = resolveFixture("simple");
+        const sourceGraph = new SourceGraph(fixturePath);
+
+        return expect(
+          () =>
+            sourceGraph.populate([
+              path.resolve(fixturePath, "test/bar.spec.js")
+            ]),
+          "to be fulfilled"
+        ).then(() => {
+          expect(
+            sourceGraph.query({
+              type: "relations",
+              to: path.resolve(fixturePath, "bar.js")
+            }),
+            "to satisfy",
+            [
+              {
+                from: path.resolve(fixturePath, "test/bar.spec.js"),
+                to: path.resolve(fixturePath, "bar.js")
+              }
+            ]
+          );
+        });
+      });
     });
 
-    it("should list incoming relations", async () => {
+    it("should find files required by tests", () => {
       const fixturePath = resolveFixture("simple");
       const sourceGraph = new SourceGraph(fixturePath);
 
-      // pass the list of test files mocha would run to populate
-      await sourceGraph.populate([
-        path.resolve(fixturePath, "test/bar.spec.js")
-      ]);
+      return expect(() => {
+        // pass the list of test files mocha would run to populate
+        return sourceGraph.populate([
+          path.resolve(fixturePath, "test/bar.spec.js")
+        ]);
+      }, "to be fulfilled").then(() => {
+        const file = sourceGraph.query({
+          type: "file",
+          path: path.resolve(fixturePath, "foo.js")
+        });
 
-      const file = sourceGraph.query({
-        type: "file",
-        path: path.resolve(fixturePath, "foo.js")
+        return expect(file.incomingRelations, "to satisfy", [
+          { from: path.resolve(fixturePath, "bar.js") }
+        ]);
       });
-
-      return expect(file.incomingRelations, "to satisfy", [
-        { from: path.resolve(fixturePath, "bar.js") }
-      ]);
     });
   });
 });
